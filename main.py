db86afcb95bf9eb477f83052a038e2221971a1a8
luoman <roman.sukach@outlook.com>
2024-01-29 23:12:31 +0000

    initial

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-import pandas as pd
 import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+import time
 
 
+np.random.seed(42)
 
+_start = time.time()
+
+size, features = 1_000_000, 20
+dataset = np.random.randn(size, features)
+target = np.random.randn(size)
+
+reg = RandomForestRegressor(max_depth=32, random_state=42, n_jobs=30)
+reg.fit(dataset, target)
+
+print(f'Time spent: {time.time() - _start}')

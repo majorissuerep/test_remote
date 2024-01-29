59963e81ab27ba1a4adec7bce043809c76227fef
luoman <roman.sukach@outlook.com>
2024-01-29 23:16:23 +0000

    initial

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ np.random.seed(42)
 
 _start = time.time()
 
-size, features = 1_000_000, 20
+size, features = 1_000_00, 20
 dataset = np.random.randn(size, features)
 target = np.random.randn(size)
 
-reg = RandomForestRegressor(max_depth=32, random_state=42, n_jobs=30)
+reg = RandomForestRegressor(n_estimators=192*4, max_depth=32, random_state=42, n_jobs=30)
 reg.fit(dataset, target)
 
 print(f'Time spent: {time.time() - _start}')
